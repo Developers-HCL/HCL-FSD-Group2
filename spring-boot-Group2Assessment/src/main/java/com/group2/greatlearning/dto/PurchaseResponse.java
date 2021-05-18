7ff3ebaf2ff3d146b6fa26942adb52dc6d100e9d
pavankarnati24@gmail.com <mvnnaveen98@gmail.com>
2021-05-18 18:29:12 +0530

    backend commit

---
+++ spring-boot-Group2Assessment/src/main/java/com/group2/greatlearning/dto/PurchaseResponse.java
@@ -0,0 +1,16 @@
+package com.group2.greatlearning.dto;
+
+
+public class PurchaseResponse {
+
+	 private final String orderTrackingNumber="";
+
+		public PurchaseResponse(String orderTrackingNumber2) {
+			// TODO Auto-generated constructor stub
+		}
+
+		public String getOrderTrackingNumber() {
+			return orderTrackingNumber;
+		}
+
+}
---
+++ spring-boot-Group2Assessment/src/test/java/com/group2/greatlearning/SpringBootResteasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.greatlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResteasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
